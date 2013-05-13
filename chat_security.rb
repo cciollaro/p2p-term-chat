d2d03fc5b3ee7ef0a419b8a09d7726d677b4ab83
Chris Ciollaro <cciollaro@gmail.com>
2013-05-13 18:55:13 -0400

    changed encapsulation on ChatSecurity

--- chat_security.rb
+++ chat_security.rb
@@ -1,28 +1,24 @@
 require 'openssl'
 
-class ChatSecurity
-	attr_accessor :private_key, :public_key, :partner_public_key
-	
+class ChatSecurity	
 	def initialize(partner_pub)
 		@partner_public_key = partner_pub
-		
+		prev_dir = Dir.pwd
 		dir = "#{Dir.home}/.rube_chat"
 
 		unless File.exists?(dir) && File.directory?(dir)
 			Dir.mkdir(dir)
 		end
 
-		Dir.chdir(dir)
 
-
-		if File.exists?('chat_key') && File.exists?('chat_key.pub')
-			@private_key = OpenSSL::PKey::RSA.new(File.open("#{Dir.home}/.rube_chat/chat_key"))
-			@public_key = OpenSSL::PKey::RSA.new(File.open("#{Dir.home}/.rube_chat/chat_key.pub"))
+		if File.exists?("#{dir}/chat_key") && File.exists?("#{dir}/chat_key.pub")
+			@private_key = OpenSSL::PKey::RSA.new(File.open("#{dir}/chat_key"))
+			@public_key = OpenSSL::PKey::RSA.new(File.open("#{dir}/chat_key.pub"))
 		else
 			@private_key = OpenSSL::PKey::RSA.new(2048)
 			@public_key = private_key.public_key
-			File.open('chat_key', 'w') {|file| file.write(@private_key)}
-			File.open('chat_key.pub', 'w') {|file| file.write(@public_key)}
+			File.open("#{dir}/chat_key", 'w') {|file| file.write(@private_key)}
+			File.open("#{dir}/chat_key.pub", 'w') {|file| file.write(@public_key)}
 		end
 	end
 	
@@ -37,4 +33,7 @@ class ChatSecurity
 		in_message = @private_key.private_decrypt(in_message)
 		return in_message
 	end
+	
+	private
+	attr_accessor :private_key, :public_key, :partner_public_key
 end
