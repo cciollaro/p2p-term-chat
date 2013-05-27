1151408cd5c9da3a1c7292b11638ff2a8d3f182c
Chris Ciollaro <cciollaro@gmail.com>
2013-05-27 04:23:03 -0400

    refactored and added error handling for the most common scenarios

--- client.rb
+++ client.rb
@@ -1,25 +1,34 @@
 require 'socket'
 require 'readline'
 
+CLEAR_CUR_LINE = "\x1b[2K\x1b[0G"
 hostname = 'localhost'
 port = 8888
+your_name = 'you'
+their_name = 'anon'
+server = TCPSocket.open(hostname, port)
 
-s = TCPSocket.open(hostname, port)
-
-include Readline
-CLEAR_CUR_LINE = "\x1b[2K\x1b[0G"
-
-name = "YOU"
-
+#listener thread
 Thread.new do
-	while msg = s.gets
-        print CLEAR_CUR_LINE
-        puts "ANON: " + msg
-        print name + ": " + Readline.line_buffer
+	while mesg = server.gets
+		print CLEAR_CUR_LINE
+		puts "#{their_name}: #{mesg}"
+		print "#{your_name}: #{Readline.line_buffer}"
 	end
 end
 
+begin
 
-loop do
-	s.puts readline(name + ": ", true)
+	while msg = Readline::readline("#{your_name}: ", true)
+		server.puts msg
+	end
+	
+rescue Interrupt
+									#try to tell the server you've left
+	server.puts 'peacin' rescue nil	#but don't worry if you can't
+rescue Errno::EPIPE
+	puts 'server disconnected'
+ensure
+	print CLEAR_CUR_LINE
+	puts 'bye'
 end
